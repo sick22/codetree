11348399e7e195cf17c8053424e9a3f8820b50b4
JungYongSeok <jeongyongseok.dev@gmail.com>
2025-05-30 16:51:46 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250530/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << fixed;
+    cout.precision(6);
+
+	int a = 13;
+    double moon = 0.165;
+
+    cout << a << " * " << moon << " = " << moon * a;
+	return 0;
+
+}
+
+
