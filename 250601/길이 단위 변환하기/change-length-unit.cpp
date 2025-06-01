442f720eb7a86be69ecce8feda8b5136ddb40030
JungYongSeok <jeongyongseok.dev@gmail.com>
2025-06-01 20:10:03 +0900

    Title: 길이 단위 변환하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250601/길이 단위 변환하기/change-length-unit.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout << "9.2ft = 280.4cm" <<endl;
+    cout << "1.3mi = 209214.2cm" <<endl;
+    return 0;
+}
