cb4f2c53118136d44560036573567cf2dd93c3f7
JungYongSeok <jeongyongseok.dev@gmail.com>
2025-06-16 19:07:15 +0900

    Title: 정수 명령 처리, Time: 16ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250616/정수 명령 처리/process-numeric-commands.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int N;
+string command[10000];
+int value[10000];
+
+
+int main() {
+    stack<int> s;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> command[i];
+        if (command[i] == "push") {
+            cin >> value[i];
+            s.push(value[i]);
+        }
+        else if(command[i] == "pop"){
+            cout << s.top() << endl;
+            s.pop();
+        }
+        else if(command[i] == "size"){
+            cout << s.size() << endl;
+        }
+        else if(command[i] == "empty"){
+            cout << s.empty() << endl;
+        }
+        else{
+            cout << s.top() << endl;
+        }
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
